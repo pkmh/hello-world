d71a024c07d12306ffef93afb3cabb301731ba83
Patrick Hickey <patrickhickey95@gmail.com>
2023-02-04 22:46:12 -0500

    shell scripts for hello world

---
+++ shell/rainbow-hello.sh
@@ -0,0 +1,17 @@
+# !/usr/bin/env bash
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+
+COLOR_RESET='\033[0m'
+
+echo "${RED}Hello, World!${COLOR_RESET}"
+# orange
+echo "${YELLOW}Hello, World!${COLOR_RESET}"
+echo "${GREEN}Hello, World!${COLOR_RESET}"
+echo "${BLUE}Hello, World!${COLOR_RESET}"
+echo "${PURPLE}Hello, World!${COLOR_RESET}"
+exit 0
