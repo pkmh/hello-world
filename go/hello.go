a4f0bccec69c74b358b70635561b41405b367497
Patrick Hickey <patrickhickey95@gmail.com>
2024-01-23 01:12:15 -0500

    hello world in go lang

---
+++ go/hello.go
@@ -0,0 +1,10 @@
+package main // making package for standalone executable
+import "fmt" // importing 'format' package   
+
+func main() {
+	/*
+	 * This is a multi line comment! Below is Hello World in English, Greek,
+	 * Japanese and Chinese respectively
+	*/
+    fmt.Println("Hello, World!\n Καλημέρα κόσμε\n こんにちは\n 世界");
+}
